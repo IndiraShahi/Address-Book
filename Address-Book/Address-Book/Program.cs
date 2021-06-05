b9911e1d13832659a721a13ab8b14a215d9d3277
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-05 21:29:31 +0530

    [Indira] ADDED: Created a contact with firstname,lastname,address,city,state,zip,phone number and email.

--- Address-Book/Address-Book/Program.cs
+++ Address-Book/Address-Book/Program.cs
@@ -7,6 +7,28 @@ namespace Address_Book
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to address book");
+            // creating object
+            AddressBook book = new AddressBook();
+
+            // calls set accessor of the properties
+            book.FirstName = "Indira";
+            book.LastName = "Shahi";
+            book.Address = "Rachenhalli";
+            book.City = "Bengaluru";
+            book.State = "Karnataka";
+            book.Zip = 560077;
+            book.PhoneNumber = 1234567890;
+            book.Email = "indirashahi@gmail.com";
+
+            // displaying values
+            Console.WriteLine("First Name : " + book.FirstName);
+            Console.WriteLine("Last Name : " + book.LastName);
+            Console.WriteLine("Address : " + book.Address);
+            Console.WriteLine("City : " + book.City);
+            Console.WriteLine("State : " + book.State);
+            Console.WriteLine("Zip : " + book.Zip);
+            Console.WriteLine("Phone Number : " + book.PhoneNumber);
+            Console.WriteLine("Email : " + book.Email);
         }
     }
 }
